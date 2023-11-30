e3a2463b8f846172e8979d4513af9d09a0f7c551
benztimm <ebuddharuksa@sfsu.edu>
2023-11-30 13:09:30 -0800

    code in vm

--- kmp_omp.cpp
+++ kmp_omp.cpp
@@ -2,68 +2,143 @@
 #include <vector>
 #include <iostream>
 #include <omp.h>
-const char* kmp_desc = "Basic implementation of KMP.";
+const char* kmp_desc = "Openmp implementation of KMP.";
 
+/*
+void computeNewNextArray(const std::string& pat, std::vector<int>& newnext) {
+    int M = pat.size();
+    newnext.resize(M + 1);
+    newnext[0] = newnext[1] = 0; // Assuming newnext[0] is not used as we start from index 1
 
-// Fills lps[] for given patttern pat[]
-void computeLPSArray(const std::string& pat, int M, std::vector<int>& lps) {
-    int length = 0;
-    lps[0] = 0;
-
-    int i = 1;
-    while (i < M) {
-        if (pat[i] == pat[length]) {
-            length++;
-            lps[i] = length;
-            i++;
-        } else {
-            if (length != 0) {
-                length = lps[length - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
+    int j = 0;
+    #pragma omp parallel for
+    for (int i = 1; i < M; i++) {
+        while (j > 0 && pat[i] != pat[j]) {
+            j = newnext[j];
         }
+        if (pat[i] == pat[j]) {
+            j++;
+        }
+        newnext[i + 1] = j;
     }
 }
 
-// KMP search algorithm
 void KMPSearch(const std::string& pat, const std::string& txt) {
-    int M = pat.length();
-    int N = txt.length();
-    int count = 0;
+    int M = pat.size();
+    int N = txt.size();
+    std::vector<int> newnext;
+    computeNewNextArray(pat, newnext);
 
-    std::vector<int> lps(M);
-    computeLPSArray(pat, M, lps);
-
+    int j = 0; // index for pat (1-based in pseudocode, adjusted to 0-based here)
+    std::vector<int> match(N, 0); // To keep track of matches
+    int maxprefixlen = 0; // To track the max prefix length
     #pragma omp parallel for
-    for (int start = 0; start < N; start += M) {
-        int end = std::min(start + M + M - 1, N); // Overlapping chunk
+    for (int i = 0; i < N; i++) { // Loop through text
+        while (j > 0 && pat[j] != txt[i]) {
+            j = newnext[j];
+        }
+        if (pat[j] == txt[i]) {
+            j++;
+        }
+        if (j == M) { // Full match found
+            match[i - M + 1] = 1;
+            j = newnext[M]; // Reset j, but this line might need adjustment
+        }
+        maxprefixlen = std::max(maxprefixlen, j);
+    }
 
-        int i = start;
-        int j = 0;
-        while (i < end) {
+    // Print the positions of matches
+    
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++) {
+        if (match[i] == 1) {
+            //std::cout << "Pattern found at index " << i << std::endl;
+        }
+    }
+    
+    std::cout << "Maximum prefix length: " << maxprefixlen - 1 << std::endl; // Adjusted back to 1-based indexing for output
+}
+*/
+
+void computeNewNextArray(const std::string& pat, std::vector<int>& newnext) {
+    int M = pat.size();
+    newnext.resize(M + 1);
+    newnext[0] = newnext[1] = 0; 
+
+    int j = 0;
+    for (int i = 1; i < M; i++) {
+        while (j > 0 && pat[i] != pat[j]) {
+            j = newnext[j];
+        }
+        if (pat[i] == pat[j]) {
+            j++;
+        }
+        newnext[i + 1] = j;
+    }
+}
+
+void KMPSearch(const std::string& pat, const std::string& txt) {
+    int M = pat.size();
+    int N = txt.size();
+    std::vector<int> newnext;
+    computeNewNextArray(pat, newnext);
+
+    std::vector<int> match(N, 0); // To keep track of matches
+
+    int numThreads = omp_get_max_threads();
+    //std::cout << "Number of threads: " << numThreads << std::endl;
+    //std::vector<int> maxPrefixLen(numThreads, 0); // To track the max prefix length for each thread
+    // Determine chunk boundaries
+    std::vector<int> chunkStart(numThreads), chunkEnd(numThreads);
+    int chunkSize = N / numThreads;
+    for (int t = 0; t < numThreads; ++t) {
+        chunkStart[t] = t * chunkSize;
+        chunkEnd[t] = (t == numThreads - 1) ? N : (t + 1) * chunkSize;
+
+        // Adjust start and end to avoid splitting words
+        if (chunkStart[t] != 0 && txt[chunkStart[t] - 1] != ' ') {
+            while (chunkStart[t] < N && txt[chunkStart[t]] != ' ') {
+                ++chunkStart[t];
+            }
+        }
+        if (chunkEnd[t] != N && txt[chunkEnd[t] - 1] != ' ') {
+            while (chunkEnd[t] < N && txt[chunkEnd[t]] != ' ') {
+                ++chunkEnd[t];
+            }
+        }
+    }
+    //int count = 0;
+    #pragma omp parallel
+    {
+        int threadId = omp_get_thread_num();
+        int start = chunkStart[threadId];
+        int end = chunkEnd[threadId];
+        //std::cout << "Thread " << threadId << " working on " << start << " to " << end << std::endl;
+
+        int j = 0; // Index for pattern
+        for (int i = start; i < end; i++) {
+            while (j > 0 && pat[j] != txt[i]) {
+                j = newnext[j];
+            }
             if (pat[j] == txt[i]) {
                 j++;
-                i++;
             }
-
             if (j == M) {
-                //#pragma omp critical
-                {
-                    //std::cout << "Found pattern at index " << std::to_string(i - j) << std::endl;
-                    count++;
-                }
-                j = lps[j - 1];
-            } else if (i < end && pat[j] != txt[i]) {
-                if (j != 0) {
-                    j = lps[j - 1];
-                } else {
-                    i++;
-                }
+                match[i - M + 1] = 1;
+                //count++;
+                j = newnext[M];
             }
         }
     }
-
-    std::cout << "Total pattern found: " << count << std::endl;
+    /*
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            if (match[i] == 1) {
+                std::cout << "Pattern found at index " << i << std::endl;
+        }
+    }
+    */
+    //std::cout << "Total matches: " << count << std::endl;
 }
+
+
